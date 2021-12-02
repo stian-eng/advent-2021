008e55db4ce646cf93bdf30b8b8eeff8cb30c79b
Stian Eng <stian@uw.edu>
2021-12-01 22:05:03 -0800

    day 2 python

--- stian/day_two/python/day_two.py
+++ stian/day_two/python/day_two.py
@@ -2,26 +2,48 @@ data = []
 test_data = []
 with open('day_two.txt', 'r') as f :
     for line in f.readlines() :
-        # CASTING AS INT MAYBE THATS THE PROB??
-        data.append(int(line.strip('\n')))
+        data.append(line.strip('\n'))
 
 with open("test.txt", 'r') as f :
     for line in f.readlines() :
-        test_data.append(int(line.strip('\n')))
+        test_data.append(line.strip('\n'))
 
 # GUT CHECK
 print(test_data, len(test_data))
 print(data[0], data[1], data[-1], len(data))
 
 def part_one(data) :
-    pass
-
+    horizontal = 0
+    depth = 0
+    for i in data :
+        direction, num = i.split()
+        if direction == 'forward' :
+            horizontal += int(num)
+        elif direction == 'up' :
+            depth -= int(num)
+        else :
+            depth += int(num)
+        
+    return horizontal * depth
 
 def part_two(data) :
-    pass
+    horizontal = 0
+    depth = 0
+    aim  = 0
+    for i in data :
+        direction, num = i.split()
+        if direction == 'forward' :
+            horizontal += int(num)
+            depth += aim * int(num)
+        elif direction == 'up' :
+            aim -= int(num)
+        else :
+            aim += int(num)
+        
+    return horizontal * depth
 
 if __name__ == '__main__' :
-    print("part 1:", part_one(data))
-    print("part 1 TEST:", part_one(test_data))
-    print("part 2:", part_two(data))
-    print("part 2 TEST:", part_two(test_data))
+    print("PART 1:", part_one(data))
+    print("PART 1 TEST INPUT:", part_one(test_data))
+    print("PART 2:", part_two(data))
+    print("PART 2 TEST:", part_two(test_data))
